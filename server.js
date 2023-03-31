312e40d06a1a2371a75fe3d18729a8a0fce6c42e
Juan Aviega <juancruzaviega@gmail.com>
2023-03-30 21:03:16 -0300

    tercera entrega

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
+import config from './src/config/config.js';
 import dotenv from 'dotenv';
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import handlebars from 'express-handlebars';
 import initializeStrategies from './src/config/passport-config.js';
 import mongoose from 'mongoose';
-import MongoStore from 'connect-mongo';
+import nodemailer from 'nodemailer';
 import passport from 'passport';
 import path from 'path';
 import pkg from 'winston';
 import router from './src/routes/info.js'
-import routerApi from './src/routes/api/randoms.js';
 import routerPost from './src/routes/posts/usersPost.js';
+import routes from './src/routes/routes.js';
+import routesCarrito from './src/routes/carritoroute.js';
 import session from 'express-session';
 import viewUser from './src/routes/viewUsers.js'
 import __dirname from './utils.js';
@@ -19,19 +22,19 @@ dotenv.config()
 const { logger } = pkg;
 const app = express();
 const PORT = process.env.PORT || 8080;
-const MONGO_URL = process.env.MONGO_URL;
+// const MONGO_URL = process.env.MONGO_URL;
 const connection = mongoose.connect(MONGO_URL);
 
-app.use(session({
-    store: MongoStore.create({
-        mongoUrl: MONGO_URL,
-        mongoOptions: { useNewUrlParser: true, useUnifiedTopology: true },
-    }),
-    secret: 'tienda',
-    resave: false,
-    saveUninitialized: false,
-    cookie: { maxAge: 30000 }
-}))
+// app.use(session({
+//     store: MongoStore.create({
+//         mongoUrl: MONGO_URL,
+//         mongoOptions: { useNewUrlParser: true, useUnifiedTopology: true },
+//     }),
+//     secret: 'tienda',
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: { maxAge: 30000 }
+// }))
 
 initializeStrategies();
 app.use(passport.initialize());
@@ -52,11 +55,14 @@ app.use(express.static(`${__dirname}/src/assets`));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
 
 app.use('/', viewUser)
 app.use('/api/sessions', routerPost);
-app.use('/api/randoms', routerApi);
+// app.use('/api/randoms', routerApi);
 app.use('/info', router);
+app.use(routes);
+app.use(routesCarrito);
 
 app.use(addLoger);
 
---
+++ src/config/config.js
@@ -0,0 +1,11 @@
+export default {
+    mongo: {
+        URL: "mongodb+srv://juanaviega:tyrone@tienda.g8gyjpy.mongodb.net/test"
+    },
+    jwt: {
+        COOKIE: process.env.JWT_COOKIE,
+        SECRET: process.env.JWT_SECRET
+    },
+    twilio: {},
+    nodemailer: {}
+}
